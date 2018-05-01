be41d9a9ed785876c3e7fce906e24b99fa643161
zpershuai <767150435@qq.com>
2018-05-01 17:21:05 +0800

    modify screen start

--- layers/zilongshanren-ui/config.el
+++ layers/zilongshanren-ui/config.el
@@ -21,7 +21,7 @@
 ;; more useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
-      '("" " zpershuai - "
+      '("" ""
         (:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name)) "%b"))))
 
